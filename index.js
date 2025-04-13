2857074c1c40db58be147e11edbe79574865ae91
zshaffer1213 <110684535+zshaffer1213@users.noreply.github.com>
2025-04-13 14:47:43 -0400

    UI update
    Added 2 buttons and functionality

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let titleEl = document.getElementById("title-el")
 let count = 0
+let totalCounter = 0
 
 function increment() {
     count += 1
+    totalCounter +=1
     countEl.textContent = count
+    titleEl.textContent = "Out Counter"
 } 
 
 function save() {
@@ -12,4 +16,18 @@ function save() {
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
+    titleEl.textContent = "Out Counter"
+}
+
+function reset() {
+    count = 0
+    totalCounter = 0
+    saveEl.textContent  = "Previous entries: "
+    titleEl.textContent = "Out Counter"
+    countEl.textContent = count
+}
+
+function totalCount() {
+    countEl.textContent = totalCounter
+    titleEl.textContent = "Total Outs"
 }
